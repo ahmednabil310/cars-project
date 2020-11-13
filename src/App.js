293670c3736251628436cc97555164fc11829bd2
unknown <an30458@gmail.com>
2020-11-13 15:19:15 +0200

    finished home page header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Nav from './components/Nav';
-
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const { default: Header } = require('../components/HomePageComponents/Header');
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Home;
