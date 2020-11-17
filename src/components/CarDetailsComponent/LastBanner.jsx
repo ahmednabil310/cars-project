376aa6f7cce7902cfcb3ef05f7bce3892cb32e08
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-11-18 01:34:01 +0200

    fixing car details
    fixing container & progress bar responsive design

--- src/components/CarDetailsComponent/LastBanner.jsx
+++ src/components/CarDetailsComponent/LastBanner.jsx
@@ -1,32 +1,34 @@
 import '../../styles/DealerPageStyles/LastBanner.css';
 const LastBanner = () => {
-  return (
-    <div className="last-banner-container d-flex">
-      <div className="row w-100">
-        <div className="col-lg-5">
-          <h1 className="banner-title">
-            <span>Approximate Price</span>
-            <br />
-            for each model
-          </h1>
-        </div>
-        <div className="col-lg-7 d-flex banner-content-container">
-          <div className="banner-price">
-            <h2 className="price-detail">Popular</h2>
-            <h3 className="price-range">$ 50,000</h3>
-          </div>
-          <div className="banner-price">
-            <h2 className="price-detail">Sport</h2>
-            <h3 className="price-range">$ 50,000</h3>
-          </div>
-          <div className="banner-price">
-            <h2 className="price-detail">Ultimate</h2>
-            <h3 className="price-range">$ 50,000</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='last-banner d-flex py-5'>
+			<div className='container py-5'>
+				<div className='row'>
+					<div className='col-lg-5 d-flex justify-content-start align-items-center'>
+						<h1 className='banner-title'>
+							<span>Approximate Price</span>
+							<br />
+							for each model
+						</h1>
+					</div>
+					<div className='col-lg-7 d-flex banner-content container'>
+						<div className='banner-price'>
+							<h2 className='price-detail'>Popular</h2>
+							<h3 className='price-range'>$ 50,000</h3>
+						</div>
+						<div className='banner-price'>
+							<h2 className='price-detail'>Sport</h2>
+							<h3 className='price-range'>$ 50,000</h3>
+						</div>
+						<div className='banner-price'>
+							<h2 className='price-detail'>Ultimate</h2>
+							<h3 className='price-range'>$ 50,000</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LastBanner;
