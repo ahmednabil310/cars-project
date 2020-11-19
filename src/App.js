7523b577235a265ec08ab571e6245b419b8ffd29
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-11-19 02:31:35 +0200

    create dealer details page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Nav from './components/Nav';
 import Dealer from './pages/Dealer';
 import Home from './pages/Home';
 import NewCarPricing from './pages/NewCarPricing';
+import DealerDetails from './pages/DealerDetails';
+
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/newcarpricing" component={NewCarPricing} />
-        <Route path="/cardetail" component={Dealer} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Nav />
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route path='/newcarpricing' component={NewCarPricing} />
+				<Route path='/cardetail' component={Dealer} />
+				<Route path='/dealer-details' component={DealerDetails} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
