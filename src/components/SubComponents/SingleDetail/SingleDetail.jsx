f76f0c2fbd40e6c33fba39389dd9b23dfd58252d
ahmed <an30458@gmail.com>
2020-12-28 23:50:27 +0200

    Editing photos

--- src/components/SubComponents/SingleDetail/SingleDetail.jsx
+++ src/components/SubComponents/SingleDetail/SingleDetail.jsx
@@ -1,6 +1,6 @@
 import ReactStars from 'react-rating-stars-component';
-import infoIcon from '../../../images/info.png'
-
+import infoIcon from '../../../images/info.png';
+import rightIcon from '../../../images/right-icon.png';
 // the hook
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
@@ -13,9 +13,13 @@ const SingleDetail = (props) => {
       {props.hasStar ? (
         <div className="single-detail d-flex ">
           <div className="d-flex align-items-center">
-            <i className="fas fa-angle-right"></i>
+            <img src={rightIcon} alt="rightIcon" />
             <h5 className="mb-0">{t(props.title)}</h5>
-            {props.info?<Link to='/buyer-guide' className='ml-2'><img width='15px'src={infoIcon} /></Link>:null}
+            {props.info ? (
+              <Link to="/buyer-guide" className="ml-2">
+                <img width="15px" src={infoIcon} />
+              </Link>
+            ) : null}
           </div>
 
           <div className="stars-container d-flex pl-4 py-2">
@@ -26,30 +30,31 @@ const SingleDetail = (props) => {
             {props.rate === null ? (
               <span className="not-stars">N/A</span>
             ) : (
-                <ReactStars
-                  edit={false}
-                  count={5}
-                  size={24}
-                  activeColor="#d53535"
-                  value={parseInt(props.rate)}
-                />
-              )}
+              <ReactStars
+                edit={false}
+                count={5}
+                size={24}
+                activeColor="#d53535"
+                value={parseInt(props.rate)}
+              />
+            )}
           </div>
         </div>
       ) : (
-          <div className="single-detail d-flex ">
-            <div className="d-flex align-items-center">
-              <i className="fas fa-angle-right"></i>
-              <h5 className="mb-0">{t(props.title)}</h5>
-            </div>
-            <div className="stars-container">
-              <p className="main__gary pl-4 py-2 mb-0">
-                {props.Specifications}
-                <strong>{props.SpecificationsValue}</strong>
-              </p>
-            </div>
+        <div className="single-detail d-flex ">
+          <div className="d-flex align-items-center">
+            {/* <i className="fas fa-angle-right"></i> */}
+            <img src={rightIcon} alt="rightIcon" />
+            <h5 className="mb-0">{t(props.title)}</h5>
           </div>
-        )}
+          <div className="stars-container">
+            <p className="main__gary pl-4 py-2 mb-0">
+              {props.Specifications}
+              <strong>{props.SpecificationsValue}</strong>
+            </p>
+          </div>
+        </div>
+      )}
       <hr />
     </div>
   );
