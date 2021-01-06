2ff2239cefd71f271e1189ea99bfe791f5fe5ee1
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-01-07 00:30:07 +0200

    Top Participants

--- src/components/TopParticipantItem/TopParticipantItem.js
+++ src/components/TopParticipantItem/TopParticipantItem.js
@@ -8,17 +8,21 @@ const TopParticipantItem = ({
 	totalPost,
 }) => {
 	return (
-		<div className="TopParticipantItem d-flex align-items-center col-10 col-sm-6 col-md-4 col-lg-3 col-xl-2 mx-2 my-2 p-2 px-3">
-			<div className="row">
+		<div className="TopParticipantItem d-flex align-items-center col-10 col-sm-6 col-md-4 col-lg-3 col-xl-2 mx-2 my-2">
+			<div className="row w-100">
 				<div className="col-5 col-lg-5 col-xl-6 image__container d-flex justify-content-center align-items-center">
 					<img className="img-fluid" src={profilePic} alt={fullName} />
 				</div>
-				<div className="col-7 col-lg-7 col-xl-6 d-flex justify-content-start align-items-center pl-0">
-					<div className="d-flex flex-column justify-content-between align-items-center">
-						<div className="TopParticipantItem__title font-weight-bold mb-1 w-100">
-							{fullName}
+				<div className="col-7 col-lg-7 col-xl-6 d-flex justify-content-start align-items-center p-0">
+					<div className="d-flex flex-column justify-content-between align-items-center w-100">
+						<div className="TopParticipantItem__title font-weight-bold w-100">
+							{fullName.split(' ')[0] === 'Mohammed' ||
+							fullName.split(' ')[1] === 'Nawal' ||
+							fullName.split(' ')[0] === 'Tauqeer'
+								? fullName.split(' ')[1]
+								: fullName}
 						</div>
-						<div className="TopParticipantItem__total__posts w-100 mb-1">
+						<div className="TopParticipantItem__total__posts w-100">
 							total Post: <strong>{totalPost}</strong>
 						</div>
 						<div className="TopParticipantItem__joind__on w-100">
