c3cd9f7bb18b2dab49d07a1faed4e281fa068df6
ahmed <an30458@gmail.com>
2021-01-07 14:43:25 +0200

    top participant

--- src/components/TopParticipantItem/TopParticipantItem.js
+++ src/components/TopParticipantItem/TopParticipantItem.js
@@ -8,12 +8,12 @@ const TopParticipantItem = ({
   totalPost,
 }) => {
   return (
-    <div className="TopParticipantItem d-flex align-items-center col-10 col-sm-6 col-md-4 col-lg-3 col-xl-2  my-2">
+    <div className="TopParticipantItem d-flex align-items-center col-12 col-sm-6 col-md-6 col-lg-4 col-xl-2  my-2">
       <div className="row w-100">
-        <div className="col-5 col-lg-5 col-xl-6 image__container d-flex justify-content-center align-items-center">
+        <div className="col-4 col-lg-5 col-xl-5 image__container d-flex justify-content-center align-items-center">
           <img className="img-fluid" src={profilePic} alt={fullName} />
         </div>
-        <div className="col-7 col-lg-7 col-xl-6 d-flex justify-content-start align-items-center p-0">
+        <div className="col-7 col-lg-7 col-xl-7 d-flex justify-content-start align-items-center p-0 top-content">
           <div className="d-flex flex-column justify-content-between align-items-center w-100">
             <div className="TopParticipantItem__title font-weight-bold w-100">
               {fullName.split(' ')[0] === 'Mohammed' ||
--- src/components/TopReviewItem/TopReviewItem.js
+++ src/components/TopReviewItem/TopReviewItem.js
@@ -3,39 +3,38 @@ import { withRouter } from 'react-router-dom';
 import '../../styles/CarReviewItem/CarReviewItem.css';
 
 const TopReviewItem = ({
-	carImage,
-	make,
-	model,
-	subject,
-	year,
-	onClickFunc,
-	totalReviews,
-	ratingStar,
+  carImage,
+  make,
+  model,
+  subject,
+  year,
+  onClickFunc,
+  totalReviews,
+  ratingStar,
 }) => {
-	return (
-		<div className="col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2 px-1 mb-4">
-			<div className="row CarReviewItem">
-				<div className="col-12 px-0 mb-3 mt-0">
-					<img
-						className="img-fluid"
-						src={carImage}
-						alt={`${make} ${model}`}
-						onClick={onClickFunc}
-					/>
-				</div>
-				<div className="col-12">
-					<p
-						className="CarReviewItem__title topReviewTitle"
-						onClick={onClickFunc}
-					>{`${make} ${model}`}</p>
-					<p className="CarReviewItem__modelyear top__review mb-2 pb-2">
-						Model Year: <span style={{ color: '#4c545c' }}>{year}</span>
-					</p>
-					{/* <p className="CarReviewItem_reviews mb-0">{subject}</p> */}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2 px-1 mb-4">
+      <div className="row CarReviewItem">
+        <div className="col-12 px-0 mb-3 mt-0">
+          <img
+            className="img-fluid"
+            src={carImage}
+            alt={`${make} ${model}`}
+            onClick={onClickFunc}
+          />
+        </div>
+        <div className="col-12">
+          <p
+            className="CarReviewItem__title topReviewTitle"
+            onClick={onClickFunc}>{`${make} ${model}`}</p>
+          <p className="CarReviewItem__modelyear top__review mb-2 pb-2">
+            Model Year: <span style={{ color: '#4c545c' }}>{year}</span>
+          </p>
+          {/* <p className="CarReviewItem_reviews mb-0">{subject}</p> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(TopReviewItem);
