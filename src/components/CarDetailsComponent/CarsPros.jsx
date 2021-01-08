204a27ce658232422159416afa718eabec555974
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-01-08 19:25:08 +0200

    cardetails card

--- src/components/CarDetailsComponent/CarsPros.jsx
+++ src/components/CarDetailsComponent/CarsPros.jsx
@@ -5,73 +5,79 @@ import close from '../../images/dealer/close.png';
 import magic from '../../images/dealer/magic.png';
 
 class CarsPros extends Component {
-  constructor(props) {
-    super(props);
-    const good = props.data.good.split(',');
-    const bad = props.data.bad.split(',');
-    const knone = props.data.known_Problems.split('.');
-    this.state = {
-      good: good,
-      bad: bad,
-      knone: knone,
-    };
-  }
+	constructor(props) {
+		super(props);
+		const good = props.data.good.split(',');
+		const bad = props.data.bad.split(',');
+		const knone = props.data.known_Problems.split('.');
+		this.state = {
+			good: good,
+			bad: bad,
+			knone: knone,
+		};
+	}
 
-  render() {
-    return (
-      <div className="cars-pros-container">
-        <div className="row">
-          <div className="col-lg-4 col-md-12 car-pros">
-            <div className="d-flex align-items-center title-container">
-              <h3 className="cons">
-                Pros <span className="right-arrow">></span>
-              </h3>
-            </div>
-            {this.state.good.map((item, index) => {
-              return (
-                <div className="pros-content d-flex" key={index}>
-                  <img src={check} width="26px" height="26px" />
-                  <h2>{item}</h2>
-                </div>
-              );
-            })}
-          </div>
-          <div className="col-lg-4 col-md-12 car-cons">
-            <div className="d-flex align-items-center title-container">
-              <h3 className="cons">
-                Cons <span className="right-arrow">></span>
-              </h3>
-            </div>
-            {this.state.bad.map((item, index) => {
-              return (
-                <div className="pros-content" key={index}>
-                  <img src={close} width="26px" height="26px" />
-                  <h2>{item}</h2>
-                </div>
-              );
-            })}
-          </div>
-          <div className="col-lg-4 col-md-12 car-cons">
-            <div className="d-flex align-items-center title-container">
-              <h3 className="cons">
-                Knows Problems <span className="right-arrow">></span>
-              </h3>
-            </div>
-            {this.state.knone.map((item, index) => {
-              if (item !== '') {
-                return (
-                  <div className="pros-content" key={index}>
-                    <img src={magic} width="26px" height="26px" />
-                    <h2>{item}</h2>
-                  </div>
-                );
-              }
-            })}
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="cars-pros-container">
+				<div className="row">
+					<div className="col-lg-4 col-md-12 pl-0 padding-mobile">
+						<div className="car-pros">
+							<div className="d-flex align-items-center title-container">
+								<h3 className="cons">
+									Pros <span className="right-arrow">&gt;</span>
+								</h3>
+							</div>
+							{this.state.good.map((item, index) => {
+								return (
+									<div className="pros-content d-flex" key={index}>
+										<img src={check} width="26px" height="26px" />
+										<h2>{item}</h2>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+					<div className="col-lg-4 col-md-12 padding-mobile">
+						<div className="car-cons">
+							<div className="d-flex align-items-center title-container">
+								<h3 className="cons">
+									Cons <span className="right-arrow">&gt;</span>
+								</h3>
+							</div>
+							{this.state.bad.map((item, index) => {
+								return (
+									<div className="pros-content" key={index}>
+										<img src={close} width="26px" height="26px" />
+										<h2>{item}</h2>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+					<div className="col-lg-4 col-md-12 pr-0 padding-mobile">
+						<div className="car-cons">
+							<div className="d-flex align-items-center title-container">
+								<h3 className="cons">
+									Knows Problems <span className="right-arrow">&gt;</span>
+								</h3>
+							</div>
+							{this.state.knone.map((item, index) => {
+								if (item !== '') {
+									return (
+										<div className="pros-content" key={index}>
+											<img src={magic} width="26px" height="26px" />
+											<h2>{item}</h2>
+										</div>
+									);
+								}
+							})}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default CarsPros;
