562f7fdaff570d23150922a6fab03605c3474cb1
Hassan <hmibrahim1999@gmail.com>
2021-02-15 15:15:30 +0200

    Display images panel on mobile view

--- src/components/CarDetailsComponent/CarDetails.jsx
+++ src/components/CarDetailsComponent/CarDetails.jsx
@@ -26,91 +26,102 @@ const CarDetails = (props) => {
   return (
     <>
       <div className='car-details-container'>
-        <div
-          className='row'
-          style={{ border: '1px solid #eaeff5', padding: '1.5rem 0' }}
-        >
-          <div className='col-lg-2 hide-scrollbar images-panel'>
-            {carImages.map((img, index) => (
-              <div>
-                <img
-                  key={index}
-                  src={img}
-                  className='w-100 mb-2'
-                  onClick={() => {
-                    if (direction) return;
-                    if (index - currentImage > 0) setDirection('right');
-                    else if (index - currentImage < 0) setDirection('left');
-                    else setDirection('');
-                    setPrevImage(currentImage);
-                    setCurrentImage(index);
-                  }}
-                />
-                <div className={currentImage === index ? 'active' : ''}></div>
-              </div>
-            ))}
-          </div>
-          <div className='col-lg-10 col-12'>
-            <div className='current-image-container'>
-              <img
-                src={carImages[currentImage]}
-                className={
-                  'w-100 current-image ' +
-                  (direction && direction + '-enterance')
-                }
-                onClick={() => setIsOpen(true)}
-                onAnimationEnd={() => setDirection('')}
-              ></img>
-              {direction && (
-                <img
-                  src={carImages[prevImage]}
-                  className={'w-100 prev-image ' + direction + '-exit'}
-                />
-              )}
+        {props.data.image_3 ? (
+          renderCarousel()
+        ) : (
+          <div className="row">
+            <div className='col-lg-6 col-12 bigger-image bigger-image-first'>
+              <img src={props.data.image_1} alt='first-image' />
             </div>
-            <a
-              class='carousel-control left'
-              onClick={() => {
-                if (direction) return;
-                setDirection('left');
-                setPrevImage(currentImage);
-                setCurrentImage(
-                  (((currentImage - 1) % carImages.length) + carImages.length) %
-                    carImages.length
-                );
-              }}
-            >
-              <i class='fa fa-angle-left'></i>
-            </a>
-            <a
-              class='carousel-control right'
-              onClick={() => {
-                if (direction) return;
-                setDirection('right');
-                setPrevImage(currentImage);
-                setCurrentImage((currentImage + 1) % carImages.length);
-              }}
-            >
-              <i class='fa fa-angle-right'></i>
-            </a>
+            <div className='col-lg-6 col-12 bigger-image'>
+              <img src={props.data.image_2} alt='second-image' />
+            </div>
           </div>
-        </div>
+        )}
       </div>
-      <ReactBnbGallery
-        photos={carImages}
-        show={isOpen}
-        onClose={() => setIsOpen(false)}
-        activePhotoIndex={currentImage}
-      />
+      {props.data.image_3 && (
+        <ReactBnbGallery
+          photos={carImages}
+          show={isOpen}
+          onClose={() => setIsOpen(false)}
+          activePhotoIndex={currentImage}
+        />
+      )}
     </>
   );
+
+  function renderCarousel() {
+    return (
+      <div
+        className='row'
+        style={{ border: '1px solid #eaeff5', padding: '1.5rem 0' }}
+      >
+        <div className='col-lg-2 hide-scrollbar images-panel'>
+          {carImages.map((img, index) => (
+            <div>
+              <img
+                key={index}
+                src={img}
+                className='w-100 mb-2'
+                onClick={() => {
+                  if (direction) return;
+                  if (index - currentImage > 0) setDirection('right');
+                  else if (index - currentImage < 0) setDirection('left');
+                  else setDirection('');
+                  setPrevImage(currentImage);
+                  setCurrentImage(index);
+                }}
+              />
+              <div className={currentImage === index ? 'active' : ''}></div>
+            </div>
+          ))}
+        </div>
+        <div className='col-lg-10 col-12'>
+          <div className='current-image-container'>
+            <img
+              src={carImages[currentImage]}
+              className={
+                'w-100 current-image ' + (direction && direction + '-enterance')
+              }
+              onClick={() => setIsOpen(true)}
+              onAnimationEnd={() => setDirection('')}
+            ></img>
+            {direction && (
+              <img
+                src={carImages[prevImage]}
+                className={'w-100 prev-image ' + direction + '-exit'}
+              />
+            )}
+          </div>
+          <a
+            class='carousel-control left'
+            onClick={() => {
+              if (direction) return;
+              setDirection('left');
+              setPrevImage(currentImage);
+              setCurrentImage(
+                (((currentImage - 1) % carImages.length) + carImages.length) %
+                  carImages.length
+              );
+            }}
+          >
+            <i class='fa fa-angle-left'></i>
+          </a>
+          <a
+            class='carousel-control right'
+            onClick={() => {
+              if (direction) return;
+              setDirection('right');
+              setPrevImage(currentImage);
+              setCurrentImage((currentImage + 1) % carImages.length);
+            }}
+          >
+            <i class='fa fa-angle-right'></i>
+          </a>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default CarDetails;
-
-// <div className="col-lg-6 col-12 bigger-image bigger-image-first">
-// <img src={props.data.image_1} alt="first-image" />
-// </div>
-// <div className="col-lg-6 col-12 bigger-image">
-// <img src={props.data.image_2} alt="second-image" />
-// </div>
